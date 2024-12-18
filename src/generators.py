0e2c2bb4957c27b9b17c7586296263db730dd1e3
Ildan Ibatullin <rustam162606@yandex.ru>
2024-12-18 17:12:06 +0300

    создание функции transaction_descriptions ,которая возвращает описание транзакции

--- src/generators.py
+++ src/generators.py
@@ -9,3 +9,15 @@ def filter_by_currency(transactions, currency):
     for transaction in transactions:
         if transaction["operationAmount"]["currency"]["code"] == currency:
             yield transaction
+
+def transaction_descriptions(transactions):
+    """
+    Генератор, который возвращает описания транзакций.
+
+    :param transactions: Список транзакций, каждая из которых представлена словарем.
+    :yield: Описание каждой транзакции.
+    """
+    for transaction in transactions:
+        description = transaction["description"]
+        yield description
+
