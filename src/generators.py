865bdd5b09d6b01e7057f3cb2658cf56a128fdbf
Ildan Ibatullin <rustam162606@yandex.ru>
2024-12-18 17:12:35 +0300

    создание функции card_number_generator которая выдает номера банковских карт в формате ХХХХ ХХХХ ХХХХ ХХХХ

--- src/generators.py
+++ src/generators.py
@@ -21,3 +21,16 @@ def transaction_descriptions(transactions):
         description = transaction["description"]
         yield description
 
+def card_number_generator(start, stop):
+    """
+    Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX.
+
+    :param start: Начальное значение (от 1 до 9999999999999999).
+    :param stop: Конечное значение (от 1 до 9999999999999999).
+    :yield: Номера банковских карт в формате XXXX XXXX XXXX XXXX.
+    """
+    for number in range(start, stop + 1):
+        # Используем zfill для добавления ведущих нулей
+        card_number = str(number).zfill(16)
+        formatted_card_number = f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
+        yield formatted_card_number
